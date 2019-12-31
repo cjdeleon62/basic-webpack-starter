3eeaf4b79287ba28acfd58089170394ab59f762e
Christopher De Leon <cdeleon@forbes.com>
2019-12-31 09:50:47 -0500

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,50 +4,50 @@ const HtmlInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').def
 const path = require('path');
 
 module.exports = {
-	mode: "none",
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: 'index.js',
-		chunkFilename: '[id].chunk.js'
-	},
-	module: {
-		rules: [
-		{
-				test: /.jsx?$/i,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					'css-loader',
-					'postcss-loader',
-					'sass-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Handlebars Webpack Demo",
-			filename: "index.html",
-			template: "./src/index.html"
-		}),
-		new MiniCssExtractPlugin({
-			// Options similar to the same options in webpackOptions.output
-			// both options are optional
-			filename: '[name].css',
-			chunkFilename: '[id].css',
-		}),
-		new HtmlInlineCSSWebpackPlugin(),
-	],
-}
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    chunkFilename: '[id].chunk.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Basic Webpack Starter',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlInlineCSSWebpackPlugin(),
+  ],
+};
